8c55fd88ebfce5806b71b08708c0599a7bd19417
Nicola Verbeeck <nicola@chimerapps.com>
2024-03-02 22:00:43 +0100

    Add tests for errors

---
+++ test/error_test.dart
@@ -0,0 +1,18 @@
+import 'package:icc_parser/src/error.dart';
+import 'package:icc_parser/src/types/tag/color_profile_tags.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Error tests', () {
+    test('Test MissingTagException', () {
+      final exception = MissingTagException(ICCColorProfileTag.icSigAToB0Tag);
+      expect(exception.toString(),
+          "Missing required tag: ${ICCColorProfileTag.icSigAToB0Tag}");
+    });
+
+    test('Test BadSpaceLinkException', () {
+      const exception = BadSpaceLinkException();
+      expect(exception.toString(), "Bad space link");
+    });
+  });
+}
